87595a051edd179445d41be7aebb8dc25f798d9b
weikunlu <weikun.lu@gmail.com>
2012-12-07 16:24:43 +0800

    clear Main.java

--- JavaApiTest/src/idv/weikun/main/Main.java
+++ JavaApiTest/src/idv/weikun/main/Main.java
@@ -6,16 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("start program");
-
-		System.out.println("starting....");
-
-		System.out.println("modify by master....");
-
-		System.out.println("modify by weikun....");
 		
-		System.out.println("modify2 by weikun....");
-
 	}
 
 }
