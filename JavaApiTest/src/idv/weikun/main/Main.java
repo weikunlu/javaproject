5b5f0a1ee752008645cefffd681f7a2a4680f08f
Wei-Kun Lu (RD-TW) <weikun_lu@trend.com.tw>
2012-12-11 11:21:48 +0800

    updated basic concept.

--- JavaApiTest/src/idv/weikun/main/Main.java
+++ JavaApiTest/src/idv/weikun/main/Main.java
@@ -2,11 +2,38 @@ package idv.weikun.main;
 
 public class Main {
 
+	
+	private void basic(){
+		/*
+		型態類型	關鍵字	位元數	範圍
+		整數	byte	8	-128 ～ 127
+		整數	short	16	-32768 ～ 32767
+		整數	int	32	-2147483648 ～ 2147483647
+		整數	long	64	-9223372036854775808 ～ 9223372036854775807
+		浮點數	float	32	依據 IEEE 754 標準
+		浮點數	double	64	依據 IEEE 754 標準
+		布林值	boolean	1	true, flase
+		字元	char	16	'\u0000' - '\uffff'
+		*/
+		
+		int i = 5;//(default)
+		long l = 5L;
+		
+		double d = 2.0d;//(default)
+		float f = 2.0f;
+		
+		byte b_oct = 024;
+		byte b_hex = 0x24;
+		byte b_dec = 24;
+		
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		//test commit
+
 	}
 
 }
