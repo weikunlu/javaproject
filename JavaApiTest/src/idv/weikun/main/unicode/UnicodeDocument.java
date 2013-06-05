9ce480a9e066c399fdbc1f44415934d1bdaca09d
weikunlu <weikun.lu@gmail.com>
2013-06-05 23:49:37 +0800

    added unicode document.

---
+++ JavaApiTest/src/idv/weikun/main/unicode/UnicodeDocument.java
@@ -0,0 +1,211 @@
+package idv.weikun.main.unicode;
+
+
+/**
+ *	Unicode 字碼分佈表
+ *
+英文 0041-005A, 0061-007A
+中文 2E80-2FDF, 3100-312F, 3400-4DBF, 4E00-9FFF, F900-FAFF
+日文 3040-30FF, 31F0-31FF
+韓文 1100-11FF, 3130-318F, AC00-D7AF
+泰文 0E00-0E7F
+寮文 0E80-0EFF
+藏文 0F00-0FFF
+彝文 A000-A4CF
+蒙古文 1800-18AF
+緬甸文 1000-109F
+高棉文 1780-17FF
+拉丁文(Latin) 00C0-02AF, 1E00-1EFF
+希臘文(Greek) 0370-03FF, 1F00-1FFF, 2C80-2CFF
+希伯來文 (Hebrew) 0590-05FF
+阿拉伯文(Arabic) 0600-06FF, 0750-077F
+敘利亞文(Syriac) 0700-074F
+西里爾文(Cyrillic), 0400-052F
+亞美尼亞文(Armenian) 0530-058F
+孟加拉文(Bengali) 0980-09FF
+僧伽羅文(Sinhala,斯里蘭卡文) 0D80-0DFF
+喬治亞文(Georgian,英國古文) 10A0-10FF, 2D00-2D2F
+歐甘文(Ogham,愛爾蘭文) 1680-169F
+如尼文(Runic,北歐古文) 16A0-16FF
+塔納文(Thaana,一種印度文) 0780-07BF
+比哈文(Syloti Nagri,一種印度文) A800-A82F
+林布文(Limbu,一種印度文) 1900-194F
+布吉文(Buginese,一種印度文) 1A00-1A1F
+奧里雅文(Oriya,一種印度文) 0B00-0B7F
+泰米爾文(Tamil,一種印度文) 0B80-0BFF
+泰盧固文(Telugu,一種印度文) 0C00-0C7F
+卡納達文(Kannada,一種印度文) 0C80-0CFF
+天城體梵文(Devanagari,一種印度文) 0900-097F
+古爾穆基文(Gurmukhi,一種印度文) 0A00-0A7F
+古吉拉特文(Gujarati,一種印度文) 0A80-0AFF
+馬拉雅拉姆文(Malayalam,一種印度文) 0D00-0D7F
+他加祿文(Tagalog,一種菲律賓文) 1700-171F
+漢奴勞文(Hanunoo,一種菲律賓文) 1720-173F
+Buhid(一種菲律賓文) 1740-175F
+Tagbanwa(一種菲律賓文) 1760-177F
+徹羅基文(Cherokee,美國印弟安文) 13A0-13FF
+Tai Le(中緬邊境民族文) 1950-197F
+新傣文(中國少數民族文) 1980-19DF
+格拉哥里文(Glagolitic,一種斯拉夫文) 2C00-2C5F
+衣索比亞文(Ethiopic) 1200-139F, 2D80-2DDF
+提非納文(Tifinagh,一種衣索匹亞文) 2D30-2D7F
+0000-007F C0 Control and Basic Latin, 標準ANSI字元
+0080-00FF C1 Control and Latin-1 Supplement, 控制碼與拉丁文
+0100-024F Latin Extended, 拉丁文
+0250-02AF IPA Extensions, 拉丁文
+02B0-02FF Spacing Modifier Letters, 符號
+0300-036F Combining Diacritical Marks, 組合記號
+0370-03FF Greek and Coptic, 希臘文與科普特文
+0400-052F Cyrillic, 西里爾文
+0530-058F Armenian, 亞美尼亞文
+0590-05FF Hebrew, 希伯來文
+0600-06FF Arabic, 阿拉伯文
+0700-074F Syriac, 敘利亞文
+0750-077F Arabic Supplement, 阿拉伯文
+0780-07BF Thaana, 塔納文 (一種印度文)
+07C0-07FF 保留
+0900-097F Devanagari, 天城體梵文 (一種印度文)
+0980-09FF Bengali, 孟加拉文
+0A00-0A7F Gurmukhi, 古爾穆基文 (一種印度文)
+0A80-0AFF Gujarati, 古吉拉特文 (一種印度文)
+0B00-0B7F Oriya, 奧里雅文 (一種印度文)
+0B80-0BFF Tamil, 泰米爾文 (一種印度文)
+0C00-0C7F Telugu, 泰盧固文 (一種印度文)
+0C80-0CFF Kannada, 卡納達文 (一種印度文)
+0D00-0D7F Malayalam, 馬拉雅拉姆文 (一種印度文)
+0D80-0DFF Sinhala, 僧伽羅文 (斯里蘭卡文)
+0E00-0E7F Thai, 泰文
+0E80-0EFF Lao, 寮文
+0F00-0FFF Tibetan, 藏文
+1000-109F Myanmar, 緬甸文
+10A0-10FF Georgian, 喬治亞文 (英國古文)
+1100-11FF Hangul Jamo, 韓文
+1200-139F Ethiopic, 衣索比亞文
+13A0-13FF Cherokee, 徹羅基文 (美國印弟安文)
+1400-167F Unified Canadian Aboriginal Syllabics, 加拿大土語音節符號
+1680-169F Ogham, 歐甘文 (愛爾蘭文)
+16A0-16FF Runic, 如尼文 (北歐古文)
+1700-171F Tagalog, 他加祿文 (一種菲律賓文)
+1720-173F Hanunoo, 漢奴勞文 (一種菲律賓文)
+1740-175F Buhid, (一種菲律賓文)
+1760-177F Tagbanwa, (一種菲律賓文)
+1780-17FF Khmer, 高棉文
+1800-18AF Mongolian, 蒙古文
+18B0-18FF 保留
+1900-194F Limbu, 林布文 (一種印度文)
+1950-197F Tai Le, (中緬邊境民族文)
+1980-19DF New Tai Lue, 新傣文 (中國少數民族文)
+19E0-19FF Khmer Symbols, 高棉文符號
+1A00-1A1F Buginese, 布吉文 (一種印度文)
+1A20-1CFF 保留
+1D00-1DBF Phonetic Extensions, 音標符號
+1DC0-1DFF Combining Diacritical Marks Supplement, 組合記號
+1E00-1EFF Latin Extended Additional, 拉丁文
+1F00-1FFF Greek Extended, 希臘文
+2000-206F General Punctuation, 標點符號
+2070-209F Superscripts and Subscripts, 上下標符號
+20A0-20CF Currency Symbols, 貨幣符號
+20D0-20FF Combining Diacritical Marks for Symbols, 符號用組合記號
+2100-214F Letterlike Symbols, 似字母符號
+2150-218F Number Forms, 數字符號
+2190-21FF Arrows, 箭頭符號
+2200-22FF Mathematical Operators, 數學運算符號
+2300-23FF Miscellaneous Technical, 特殊符號
+2400-243F Control Pictures, 控制記號
+2440-245F Optical Character Recognition, 光學字元識別符號
+2460-24FF Enclosed Alphanumerics, 括號字母數字符號
+2500-257F Box Drawing, 表格符號
+2580-259F Block Elements, 區塊符號
+25A0-25FF Geometric Shapes, 幾何圖形符號
+2600-26FF Miscellaneous Symbols, 其他符號
+2700-27BF Dingbats, 前導符號
+27C0-27EF Miscellaneous Mathematical Symbols-A, 數學符號
+27F0-27FF Supplemental Arrows-A, 箭頭符號
+2800-28FF Braille Patterns, 盲文 (點字符號)
+2900-297F Supplemental Arrows-B, 箭頭符號
+2980-29FF Miscellaneous Mathematical Symbols-B, 數學符號
+2A00-2AFF Supplemental Mathematical Operators, 數學運算符號
+2B00-2BFF Miscellaneous Symbols and Arrows, 箭頭符號
+2C00-2C5F Glagolitic, 格拉哥里文 (一種斯拉夫文)
+2C80-2CFF Coptic, 科普特文 (一種埃及文)
+2D00-2D2F Georgian Supplement, 喬治亞文 (英國古文)
+2D30-2D7F Tifinagh, 提非納文 (一種衣索匹亞文)
+2D80-2DDF Ethiopic Extended, 衣索匹亞文
+2DE0-2DFF 保留
+2E00-2E7F Supplemental Punctuation, 標點符號
+2E80-2EFF CJK Radicals Supplement, 中文部首
+2F00-2FDF Kangxi Radicals, 中文部首
+2FF0-2FFF Ideographic Description Characters, 漢字結構描述符號
+3000-303F CJK Symbols and Punctuation, 中日韓符號
+3040-309F Hiragana, 日文平假名
+30A0-30FF Katakana, 日文片假名
+3100-312F Bopomofo, 中文注音符號
+3130-318F Hangul Compatibility Jamo, 韓文
+3190-319F Kanbun, 中文上下標
+31A0-31BF Bopomofo Extended, 中文注音符號
+31C0-31EF CJK Strokes, 中文筆劃符號
+31F0-31FF Katakana Phonetic Extensions, 日文片假名
+3200-33FF Enclosed CJK Letters and Months, 中日韓組合符號字
+3400-4DBF CJK Unified Ideographs Extension A, 中文
+4DC0-4DFF Yijing Hexagram Symbols, 易經符號
+4E00-9FFF CJK Unified Ideographs, 中文
+A000-A4CF Yi, 彝文
+A4D0-A6FF 保留
+A700-A71F Modifier Tone Letters, 音標符號
+A720-A7FF 保留
+A800-A82F Syloti Nagri, 比哈文 (一種印度文)
+A830-A8FF 保留
+AC00-D7AF Hangul Syllables, 韓文
+D780-D7FF 保留
+D800-DFFF Surrogates, UTF-16相容保留區
+E000-F8FF Private Use Area, 自造字專用區
+F900-FAFF CJK Compatibility Ideographs, 中文
+FB00-FB4F Alphabetic Presentation Forms, 字母變體顯現形式
+FB50-FDFF Arabic Presentation Forms-A, 阿拉伯文變體顯現形式
+FE00-FE0F Variation Selectors, 字型變換選取器符號
+FE10-FE1F Vertical Forms, 中文直排符號
+FE20-FE2F Combining Half Marks, 組合記號
+FE30-FE4F CJK Compatibility Forms, 中文直排符號
+FE50-FE6F Small Form Variants, 標點符號
+FE70-FEFF Arabic Presentation Forms-B, 阿拉伯文變體顯現形式
+FF00-FFEF Halfwidth and Fullwidth Forms, 半形及全形字符
+FFF0-FFFF Specials, 特殊保留區
+ *
+ */
+public class UnicodeDocument {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//output 24171
+		System.out.println((int)'幫');
+		
+		//output 5e6b
+		System.out.println(Integer.toHexString(24171));
+		
+		//output 5e6b
+		System.out.println(Integer.toHexString('幫'));
+		
+		
+		System.out.println("\u90A6 \u5e6b \u5fd9  \u87CE");  
+
+		new UnicodeDocument().travelUnicodeBetweenChars();
+	}
+
+	void travelUnicodeBetweenChars(){
+		
+		//2E80-2FDF, 3400-4DBF, 4E00-9FFF
+		char startChar = '\uE7C7';
+		char endChar = '\uE7F3';
+		int i;
+		for (i=0;startChar!=endChar;++i,startChar++){
+			System.out.println(startChar);
+		}
+		System.out.println("i: "+i);
+		
+	}
+	
+	
+}
